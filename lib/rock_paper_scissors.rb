4d519e67d9bc3fd3a379b555b52ae784903aadcd
Dan Danilyuk and Sarah Lindmar <sarah@>
2017-09-06 11:07:12 -0700

    make script executable

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,17 +1,27 @@
+#!/usr/bin/ruby
 class RPS
   def initialize()
-
   end
 
   def wins(move1, move2)
     if (move1 == 'rock' && move2 == 'scissors')
-      true
+      puts true
     elsif (move1 == 'scissors' && move2 == 'paper')
-      true
+      puts true
     elsif (move1 == 'paper' && move2 == 'rock')
-      true
+      puts true
+    elsif (move1 ==  move2)
+      puts "tie"
     else
-      false
+      puts false
     end
   end
 end
+
+example = RPS.new()
+puts "Player 1 type rock, scissors, or paper"
+move1 = gets.chomp
+puts "Player 2 type rock, scissors, or paper"
+move2 = gets.chomp
+
+example.wins(move1, move2)
